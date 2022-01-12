59388348d4d316be0e9725fc71490fc3630f3b4f
killian31 <80256683+killian31@users.noreply.github.com>
2022-01-12 17:46:22 +0100

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,26 +1,28 @@
 import smtplib
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from Confidentials import SMTP_USERNAME, SMTP_PASSWORD
 
-filename_cv = "CV_KS.pdf"
+# The documents to attach to the e-mail
+# 1st one :
+filename_cv = "your_document.pdf"
 cv = MIMEApplication(open(filename_cv, 'rb').read())
 cv.add_header('Content-Disposition', 'attachment', filename=filename_cv)
-
-filename_fiche = 'fiche_competences_Mag_1A_2021-2022.pdf'
+# 2nd one :
+filename_fiche = 'other_document.pdf'
 comp = MIMEApplication(open(filename_fiche, 'rb').read())
 comp.add_header('Content-Disposition', 'attachment', filename=filename_fiche)
 
+
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_FROM = "killian.steunou@gmail.com"
+EMAIL_FROM = "prenom.nom@gmail.com"
 
-EMAIL_SUBJECT = "Stage fin de 3A TSE"
+EMAIL_SUBJECT = "Your e-mail subject here"
 
 def send_email(NAME, EMAIL_TO):
-    EMAIL_MESSAGE = """Bonjour {},\n\nJe suis en 3ème année d’Éco-Maths à TSE et en Magistère d'Économiste Statisticien, et je recherche un stage de 3 à 4 mois débutant en mai 2022.\n\nJe suis passionné par l’Intelligence Artificielle, la Data Science et tout ce qui touche au Big Data.\nGrâce à la première année de Magistère, j’ai acquis des compétences d’analyse de données (k-means clustering, ACP, AFC…), et je maitrise très bien les langages R et Python (je réalise régulièrement des projets en Python, je m’intéresse de près à la programmation).\n\nLe stage que je recherche serait donc orienté Data Science/Analysis, et dans l’idéal me permettrait d’apprendre à utiliser un outil de Data Visualisation (comme Tableau, PowerBI ou Dataiku), compétence qu’il m’est nécessaire d’acquérir pour mener à bien mon projet professionnel.\n\nSavez-vous si mon profil pourrait intéresser votre entreprise en tant que stagiaire ?\n\nJe reste disponible pour en discuter,\nMerci pour votre aide,\n\nKillian Steunou\n\nTel : +33 6 95 74 30 22\n@ : killian.steunou@gmail.com\n\np.s. : mon cv est en pièce jointe, ainsi qu’une fiche de compétence, au cas où :)\n""".format(NAME)
+    EMAIL_MESSAGE = """Hello {},\n\nMy name is Killiand and blablabla.\n\nKind regards,\n\nKillian \n\n""".format(NAME)
     s = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     s.starttls()
     s.login(SMTP_USERNAME, SMTP_PASSWORD)
